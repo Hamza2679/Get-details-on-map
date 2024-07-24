725facd56c18ab3f380aaee2aec1cc6a1ac0a723
Hamza musema <hamzamusema26@gmail.com>
2024-07-24 12:28:21 +0300

    removing un necessary files

--- lib/src/features/map/map_providers/current_time_provider.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final currentTimeProvider = StateProvider<String>((ref) {
-  return 'Tap on the map to get the current time.';
-});
--- lib/src/features/map/map_providers/tapped_location_provider.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:latlong2/latlong.dart';
-
-final tappedLocationProvider = StateProvider<LatLng?>((ref) {
-  return null;
-});
